12648ef22bef7886db121bed11fab5af0470c05f
zeke2400 <skyreg2400@gmail.com>
2015-05-07 14:27:31 +0200

    x

--- test2/Controllers/animeController.cs
+++ test2/Controllers/animeController.cs
@@ -12,16 +12,17 @@ namespace test2.Controllers
 {
     public class animeController : Controller
     {
-        //
+         //
         // GET: /anime/
        // [HttpGet]
         public ActionResult Index()
         {
-          string result =  new HttpClient().GetStringAsync("http://services.tvrage.com/feeds/full_search.php?show=Naruto").Result;
-          XmlSerializer dims = new System.Xml.Serialization.XmlSerializer(typeof(animeapi.Results));
+            string result = new HttpClient().GetStringAsync("http://services.tvrage.com/feeds/full_search.php?show=Naruto").Result;
+            XmlSerializer dims = new System.Xml.Serialization.XmlSerializer(typeof(ResultsShow));
 
-          animeapi.Results returnclass = (animeapi.Results)dims.Deserialize(new StringReader(result));
-          return View(returnclass);
+            ResultsShow returnclass = (ResultsShow)dims.Deserialize(new StringReader(result));
+            
+            return View(returnclass);
         }
 
     }
