c736d865c4eb6c0da016d7846b218324fc1b87ae
zeke2400 <skyreg2400@gmail.com>
2015-05-07 14:42:18 +0200

    working

--- test2/Controllers/animeController.cs
+++ test2/Controllers/animeController.cs
@@ -18,11 +18,10 @@ namespace test2.Controllers
         public ActionResult Index()
         {
             string result = new HttpClient().GetStringAsync("http://services.tvrage.com/feeds/full_search.php?show=Naruto").Result;
-            XmlSerializer dims = new System.Xml.Serialization.XmlSerializer(typeof(ResultsShow));
+            XmlSerializer dims = new System.Xml.Serialization.XmlSerializer(typeof(animeapi.Results));
 
-            ResultsShow returnclass = (ResultsShow)dims.Deserialize(new StringReader(result));
+            animeapi.Results returnclass = (animeapi.Results)dims.Deserialize(new StringReader(result));
 
-          animeapi.Results returnclass = (animeapi.Results)dims.Deserialize(new StringReader(result));
           
           return View(returnclass);
         }
