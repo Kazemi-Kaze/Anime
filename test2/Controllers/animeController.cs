91f59dc486ee3c7f8b8cb0053e0a494a0735096f
zeke2400 <tessran2400>
2015-05-07 10:41:20 +0200

    https://github.com/zeke2400/Anime

---
+++ test2/Controllers/animeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Serialization;
+using test2.Models;
+
+namespace test2.Controllers
+{
+    public class animeController : Controller
+    {
+        //
+        // GET: /anime/
+       // [HttpGet]
+        public ActionResult Index()
+        {
+          string result =  new HttpClient().GetStringAsync("http://services.tvrage.com/feeds/full_search.php?show=Naruto").Result;
+          XmlSerializer dims = new System.Xml.Serialization.XmlSerializer(typeof(animeapi.Results));
+
+          animeapi.Results returnclass = (animeapi.Results)dims.Deserialize(new StringReader(result));
+          return View(returnclass);
+        }
+
+    }
+}
